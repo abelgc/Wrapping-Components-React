1952969e60f851d1d672273f87ba6794cdc90911
Abel Canizares <Abel.Canizares@derivco.co.uk>
2023-12-01 12:05:58 +0100

    Adress Button Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Button from "./components/Button";
 import Input from "./components/input";
 
 function App() {
@@ -5,6 +6,8 @@ function App() {
     <main>
       <Input id="name" label="Your name"  type="text"/>
       <Input id="age" label="Your age" type="number"/>
+      <Button el="button">A Button</Button>
+      <Button el="anchor" href="www.google.com">A Button</Button>
     </main>);
 }
 
