6643a99a9dcc8772ac8c9b1970c4a56243122168
Abel Canizares <Abel.Canizares@derivco.co.uk>
2023-12-04 11:24:55 +0100

    Extending ElementType in Wrapper Container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,15 +1,16 @@
 // polymorphic component
 // reason with this Wrapper component is to make it easier to style the container sharing css, logic and common jsx code
 import {type ReactNode, type ElementType } from 'react';
+import Button from './Button';
 
 // to use generic with componentpropswithoutref we need to make comopnentprops generic
-//incomming component identifier
-type ContainerProps<T extends ElementType> = { 
-    asProp?: T;
-    children: ReactNode;
-}& React.ComponentPropsWithoutRef<T>;
 
-export default function Container<C extends ElementType>({asProp, children}: ContainerProps<C>) {
+type ContainerProps<C extends ElementType> = { //incomming component identifier
+    asProp?: C;
+    children: ReactNode;
+}& React.ComponentPropsWithoutRef<C>;
+
+export default function Container<T extends ElementType>({asProp, children}: ContainerProps<T>) {
     const Container = asProp || 'div';
     return (
         <Container>
