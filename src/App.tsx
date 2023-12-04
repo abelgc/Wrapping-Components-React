2acbdefb6bf93321c471ba2cb08c8fed2550eb6b
Abel Canizares <Abel.Canizares@derivco.co.uk>
2023-12-04 14:39:09 +0100

    useImperativeHandle in form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+import { type FormHandle } from "./components/Form";
 import Button from "./components/Button";
 import Container from "./components/Container";
 import Input from "./components/Input";
@@ -7,17 +8,21 @@ import Form from "./components/Form";
 
 function App() {
 
+  const inputRef = useRef<HTMLInputElement>(null);
+  const customForm = useRef<FormHandle>(null);
+
   function handleSave(data:unknown){
     const extractedData = data as {name: string, age: string}; //fields in inputs can b presented number but extracted are always strings
     console.log(extractedData.name, extractedData.age);
+    customForm.current?.clear();
   }
-  const inputRef = useRef<HTMLInputElement>(null);
+
 
   return (
     <main>
       {/* <Input id="name" label="Your name"  type="text"/> */}
-      {/* <Input id="test" label="Test" ref={inputRef}/> */}
-      <Form onSave={handleSave}>
+      <Input id="test" label="Test" ref={inputRef}/>
+      <Form onSave={handleSave} ref={customForm}>
         <Input id="name" label="Your name"  type="text"/>
         <Input id="age" label="Age" type="number"/> 
         <p>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,24 +1,37 @@
-import { ComponentPropsWithoutRef, FormEvent } from "react";
+import { ComponentPropsWithoutRef, FormEvent, forwardRef, useImperativeHandle, useRef } from "react";
 
-type FormProps= ComponentPropsWithoutRef<"form"> &{
-    onSave: (value: unknown)=> void;
+type FormProps = ComponentPropsWithoutRef<"form"> & {
+    onSave: (value: unknown) => void;
 };
 
+export type FormHandle={
+    clear: () => void;
+}
 
-export default function Forms({onSave, children, ...otherProps}: FormProps){
+const Form = forwardRef<FormHandle, FormProps>(function Form({ onSave, children, ...otherProps }, ref) {
+    const form = useRef<HTMLFormElement>(null);
+    //function to expose callable component in another place in the app
+    useImperativeHandle(ref, () => {
+        return {
+            clear() {
+                form.current?.reset();
+            }
+        };
+    });
 
     function handleSubmit(event: FormEvent<HTMLFormElement>) {
-        event.preventDefault();      
+        event.preventDefault();
         //gather all info from input field and group them together in a data object
         const formData = new FormData(event.currentTarget); //to make this work you need name prop in input
         const data = Object.fromEntries(formData); // to access data you need to use data.name now on
         onSave(data);
-        event.currentTarget.reset();
     }
 
     return (
-        <form onSubmit={handleSubmit} {...otherProps}>
+        <form onSubmit={handleSubmit} {...otherProps} ref={form}>
             {children}
         </form>
     );
-}
+});
+
+export default Form;
