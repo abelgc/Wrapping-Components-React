65848c9c4de930f65bbc6f53b53d33841fc1ec57
Abel Canizares <Abel.Canizares@derivco.co.uk>
2023-12-04 11:04:23 +0100

    Polymorhpic Container wrapper Component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,15 +1,16 @@
 // polymorphic component
 // reason with this Wrapper component is to make it easier to style the container sharing css, logic and common jsx code
-
 import {type ReactNode, type ElementType } from 'react';
 
-type ContainerProps = {
-    asProp: ElementType;
+// to use generic with componentpropswithoutref we need to make comopnentprops generic
+//incomming component identifier
+type ContainerProps<T extends ElementType> = { 
+    asProp?: T;
     children: ReactNode;
-}
+}& React.ComponentPropsWithoutRef<T>;
 
-export default function Container({asProp, children}: ContainerProps) {
-    const Container = asProp;
+export default function Container<C extends ElementType>({asProp, children}: ContainerProps<C>) {
+    const Container = asProp || 'div';
     return (
         <Container>
            {children}
